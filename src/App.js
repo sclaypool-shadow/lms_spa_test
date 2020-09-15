62b31e1638c53928a364a95bb6957f81e4a51488
Saahil Claypool <saahil.claypool@shadowhealth.com>
2020-09-15 10:54:49 -0400

    Basic spa with fetch & cors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [currentUser, setCurrentUser] = useState('');
+  let dofetch = async () => {
+    let response = await fetch('https://localhost:3000/spa', {
+      credentials: 'include',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    let json = await response.json();
+    setCurrentUser(json['current_username'])
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dofetch()}>Fetch</button>
+      <div>
+        Current user name (click button to load): {currentUser}
+      </div>
     </div>
   );
 }
